db875c6ef3d8c4f3df42fdad640ea2cf7804a72c
Marcio Sena <marciosena.inf@gmail.com>
2019-06-28 11:25:29 -0300

    refactor: remove dotenv

--- next.config.js
+++ next.config.js
@@ -1,14 +1,8 @@
 const withPlugins = require('next-compose-plugins');
 const withSass = require('@zeit/next-sass');
 const withImages = require('next-images');
-const { parsed: localEnv } = require('dotenv').config();
-const webpack = require('webpack');
 
 const nextConfig = {
-  webpack(config) {
-    config.plugins.push(new webpack.EnvironmentPlugin(localEnv));
-    return config;
-  },
   exportPathMap: function() {
     return {
       '/': { page: '/' },
