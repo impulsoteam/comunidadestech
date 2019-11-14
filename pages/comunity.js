193ac2ab00f513f7c313edc95b994cf229fa3eaa
Leo AD <talkto@theleoad.com>
2019-11-14 10:32:06 -0300

    chore: change header logo

---
+++ pages/comunity.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import Header from 'components/Header/';
+import Footer from 'components/Footer/';
+
+class Index extends Component {
+  render() {
+    return (
+      <>
+        <Header />
+
+        <Footer />
+      </>
+    );
+  }
+}
+
+export default Index;
