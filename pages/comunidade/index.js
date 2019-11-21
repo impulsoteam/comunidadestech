c762a1053aeaf05c8d5abb4496d6f1fdcd45db0b
Leo AD <talkto@theleoad.com>
2019-11-21 09:32:38 -0300

    feat: create exception stream for communities

--- pages/comunidade/index.js
+++ pages/comunidade/index.js
@@ -52,47 +52,57 @@ const Comunity = () => {
   useEffect(() => {
     const fetchData = async () => {
       const result = await axios(API_HOST);
-      setComunity(filterComunity(normalize(result.data), router.query.name));
+      filterComunity(normalize(result.data), router.query.name)
+        ? setComunity(filterComunity(normalize(result.data), router.query.name))
+        : setComunity(null);
       setList(normalize(result.data));
     };
     fetchData();
   }, []);
 
   useEffect(() => {
-    setFilteredList(filterCity(list, comunity.city));
+    comunity && setFilteredList(filterCity(list, comunity.city));
   }, [list, comunity]);
 
   console.log(filteredList);
   return (
-    <div>
-      <ComunityHero />
-      <ComunityCard
-        name={comunity.name}
-        state={comunity.state}
-        city={comunity.city}
-        size={comunity.size}
-        category={comunity.category}
-        description={comunity.description}
-        logo={comunity.logo}
-        tags={comunity.tags}
-        link={comunity.link}
-      />
-      <div className="container related">
-        <div className="columns">
-          <div className="column isfull">
-            <h3 className="title is-5">COMUNIDADES RELACIONADAS</h3>
-          </div>
-        </div>
-        <div className="columns is-2 is-variable is-multiline">
-          {filteredList.slice(0, 3).map((card) => (
-            <div className="column is-one-third" key={card.id}>
-              <Card content={card} />
+    <>
+      {comunity ? (
+        <div>
+          <ComunityHero />
+          <ComunityCard
+            name={comunity.name}
+            state={comunity.state}
+            city={comunity.city}
+            size={comunity.size}
+            category={comunity.category}
+            description={comunity.description}
+            logo={comunity.logo}
+            tags={comunity.tags}
+            link={comunity.link}
+          />
+          <div className="container related">
+            <div className="columns">
+              <div className="column isfull">
+                <h3 className="title is-5">COMUNIDADES RELACIONADAS</h3>
+              </div>
             </div>
-          ))}
+            <div className="columns is-2 is-variable is-multiline">
+              {filteredList.slice(0, 3).map((card) => (
+                <div className="column is-one-third" key={card.id}>
+                  <Card content={card} />
+                </div>
+              ))}
+            </div>
+          </div>
+          <style jsx>{styles}</style>
         </div>
-      </div>
-      <style jsx>{styles}</style>
-    </div>
+      ) : (
+        <div className="container">
+          <h1>404 - Comunidade não existe!</h1>
+        </div>
+      )}
+    </>
   );
 };
 
