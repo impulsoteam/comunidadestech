b94689efb3aaec0f4abd8fffe6b1935e118ddad7
Julio Goncalves <julio-goncalves@outlook.com>
2019-11-22 16:13:42 -0300

    creating tests

--- tests/utils.js
+++ tests/utils.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 export const connect = async () => {
-  return mongoose.connect('mongodb://localhost/ctechTest', {
+  return mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
