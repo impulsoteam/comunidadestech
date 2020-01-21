09b6cb01f0924929d381a28074532c027c98daad
Leo AD <talkto@theleoad.com>
2020-01-21 08:14:30 -0300

    chore: prevent select zoom on mobile

--- pages/_document.js
+++ pages/_document.js
@@ -21,7 +21,10 @@ export default class MyDocument extends Document {
     return (
       <html lang="pt-br">
         <Head>
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, maximum-scale=1,user-scalable=0"
+          />
           <meta name="theme-color" content="#8c43ff" />
           <meta
             name="description"
