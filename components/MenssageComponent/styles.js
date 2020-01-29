6955efc5278a0da2b824644b59f504007f19eb81
marianaviana <immarianaviana@gmail.com>
2020-01-29 11:47:11 -0300

    fix mobile and tablet resolution

--- components/MenssageComponent/styles.js
+++ components/MenssageComponent/styles.js
@@ -37,7 +37,7 @@ export default css`
       padding-top: 24px;
     }
   }
-  @media only screen and (max-width: 700px) {
+  @media only screen and (max-width: 1023px) {
     height: 100vh;
     padding-top: 120px;
     img { max-width: 90%; }
