0225b3ec8301a0082f8add3a2789055e9dfe6818
marianaviana <immarianaviana@gmail.com>
2020-01-29 10:43:24 -0300

    apply custom error page

---
+++ pages/_error.js
@@ -0,0 +1,8 @@
+import React, { PureComponent } from 'react';
+import MenssageComponent from '/components/MenssageComponent/';
+
+const Error = () => {
+	return <MenssageComponent />;
+}
+
+export default Error;
